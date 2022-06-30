374688b86c5274646865a2ab483f69885421d051
leonardodg2084 <leonardodg2084@gmail.com>
2022-06-30 20:46:58 -0300

    App ASG + ELB

---
+++ app-asg-lb/data.tf
@@ -0,0 +1,43 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_subnet" "app_subnet" {
+  cidr_block = var.cidr_block
+}
+
+data "template_file" "mongodb_startup_script" {
+  template = file("mongodb.sh")
+  vars = {
+    "tag" = "5.0.2"
+  }
+}
+
+data "template_file" "app_startup_script" {
+  template = file("app.sh")
+  vars = {
+    image          = lookup(var.app_docker, "image")
+    tag            = lookup(var.app_docker, "tag")
+    port           = lookup(var.app_docker, "port")
+    mongodb_server = aws_instance.mongodb.private_ip
+  }
+}
+
+
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
---
+++ app-asg-lb/output.tf
@@ -0,0 +1,3 @@
+output "ip_address_mongodb" {
+  value = aws_instance.mongodb.private_ip
+}
