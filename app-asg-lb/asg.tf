4f816aef9659bd9459112c7835a2848bb044301e
leonardodg2084 <leonardodg2084@gmail.com>
2022-06-30 22:50:13 -0300

    App ASG + ELB

--- app-asg-lb/asg.tf
+++ app-asg-lb/asg.tf
@@ -57,5 +57,3 @@ resource "aws_autoscaling_attachment" "app-auto-attach" {
   autoscaling_group_name = aws_autoscaling_group.app-asg.id
   alb_target_group_arn = aws_lb_target_group.app-tg.arn
 }
-
-
